f9f51a66831055c5481ff663088bc0379e3dfba1
nglai <nglaikoin@hotmail.com>
2024-07-07 22:53:51 -0300

    carrosel

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function funcaoEnviar() {
     window.open("http://127.0.0.1:5500/index.html", "_self");
 }
 
+//Hamburguer
 const abreHamburguer = document.getElementById("hamburguer");
 const hamburguer = document.getElementById("menu_menor");
 const fechaHamburguer = document.getElementById("menu_menor_icone");
@@ -14,6 +15,17 @@ abreHamburguer.addEventListener("click", () => {
 fechaHamburguer.addEventListener("click", () => {
     hamburguer.style.display = "none";
 })
+
+
+//Carrosel
+let count = 1;
+document.getElementById("radio1").checked = true;
+setInterval(() => {
+    count++;
+    if (count > 3){count = 1;}
+    document.getElementById("radio" + count).checked = true;
+}, 8000);
+
 
 function pesquisar () {
     console.log("aaa")
